502b190ef4298ea9311d47381d6cc93ec3f39a4f
Michael Bishop <michaelabishop1988@gmail.com>
2022-03-22 11:52:14 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let question = prompt("Hello, do you like my web page?")
+
+if (question == "yes") {
+    alert("You have good taste! 😉")
+} else {
+    alert(`What do you mean ${question} 🥺, I guess you can't please everyone 😎`)
+}
